3932f48b90bd386b684af353241739d43aa9952b
Avdon Racki <avdon@racki.ca>
2019-02-09 04:12:58 -0500

    moved vrview from main to vrtour

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,28 +1,19 @@
 package com.example.myapplication;
 
-import android.content.Intent;
-import android.content.res.AssetManager;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import com.google.vr.sdk.widgets.pano.VrPanoramaView;
-import java.io.InputStream;
+
+
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
-
-    private VrPanoramaView mVRPanoramaView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +21,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        mVRPanoramaView = (VrPanoramaView) findViewById(R.id.vrPanoramaView);
-        loadPhotoSphere();
 
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -90,21 +78,5 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else {
             super.onBackPressed();
         }
-    }
-
-    private void loadPhotoSphere() {
-        VrPanoramaView.Options options = new VrPanoramaView.Options();
-        InputStream inputStream = null;
-
-        AssetManager assetManager = getAssets();
-        try {
-            inputStream = assetManager.open("vrtest.jpg");
-            options.inputType = VrPanoramaView.Options.TYPE_MONO;
-            mVRPanoramaView.loadImageFromBitmap(BitmapFactory.decodeStream(inputStream), options);
-            inputStream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
     }
 }
