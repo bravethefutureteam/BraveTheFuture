abd93d0034e553d3e42a4815ee431be8b89ccbd3
Cameron Adams <45191047+cflAdams@users.noreply.github.com>
2019-02-08 22:23:55 -0500

    Motorsport fixes?
    Also added virtual tour

--- MyApplication/app/src/main/java/com/example/myapplication/Motorsport.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Motorsport.java
@@ -24,7 +24,7 @@ public class Motorsport extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.individual_motorsports);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
