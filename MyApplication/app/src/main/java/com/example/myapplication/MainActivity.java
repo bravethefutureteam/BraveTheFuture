f61da1a68e0ec0fcddee43343cc3856c5e17ad53
HaraseesG <45112294+HaraseesG@users.noreply.github.com>
2019-02-09 02:37:43 -0500

    Revert "Revert "Menu Overlay""
    This reverts commit bbfcb77a87af91a7a1eff90b489a1f5dbc1ee7d5.

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,8 +18,8 @@ import android.view.MenuItem;
 import com.google.vr.sdk.widgets.pano.VrPanoramaView;
 import java.io.InputStream;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     private VrPanoramaView mVRPanoramaView;
 
@@ -30,26 +30,24 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent browser_intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://uoit.ca/crisis-centre/"));
-                startActivity(browser_intent);
-            }
-        });
         mVRPanoramaView = (VrPanoramaView) findViewById(R.id.vrPanoramaView);
         loadPhotoSphere();
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+    }
 
-
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     private void loadPhotoSphere() {
@@ -66,66 +64,5 @@ public class MainActivity extends AppCompatActivity
             e.printStackTrace();
         }
 
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_ConnectedCampus) {
-
-        } else if (id == R.id.nav_ExtraCurriculars) {
-            startActivity(new Intent(MainActivity.this, Motorsport.class));
-        } else if (id == R.id.nav_Information) {
-
-        } else if (id == R.id.nav_MentalHealth) {
-
-        } else if (id == R.id.nav_StudentLife) {
-
-        } else if (id == R.id.nav_TechFocus) {
-
-        } else if (id == R.id.nav_VirtualTour) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
