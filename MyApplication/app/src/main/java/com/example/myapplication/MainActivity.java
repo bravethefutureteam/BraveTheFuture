7f6fd04910fef5b95d640586769e238e32fd2a7a
HaraseesG <45112294+HaraseesG@users.noreply.github.com>
2019-02-09 03:48:56 -0500

    Menu Overlay Completed
    Still missing indicator to tell which age you are on

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -43,17 +43,39 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-//       if (savedInstanceState == null) {
-//           getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ExtraCurriculars()).commit();
-//           navigationView.setCheckedItem(R.id.nav_ExtraCurriculars);
-//       }
+
+        if (savedInstanceState == null) {
+           getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Home()).commit();
+           navigationView.setCheckedItem(R.id.nav_Home);
+       }
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item){
         switch (item.getItemId()){
+            case R.id.nav_Home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Home()).commit();
+                break;
+            case R.id.nav_Information:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Information()).commit();
+                break;
             case R.id.nav_ExtraCurriculars:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ExtraCurriculars()).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ExtraCurriculars()).commit();
+                break;
+            case R.id.nav_TechFocus:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new TechFocus()).commit();
+                break;
+            case R.id.nav_MentalHealth:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new MentalHealth()).commit();
+                break;
+            case R.id.nav_ConnectedCampus:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ConnectedCampus()).commit();
+                break;
+            case R.id.nav_StudentLife:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new StudentLife()).commit();
+                break;
+            case R.id.nav_VirtualTour:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new VirtualTour()).commit();
                 break;
         }
 
