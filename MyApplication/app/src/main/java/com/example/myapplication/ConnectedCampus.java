b092a3ae958c3169305472e36ee97e29b7dc98bb
Avdon Racki <avdon@racki.ca>
2019-02-09 11:15:59 -0500

    put vr into connected

--- MyApplication/app/src/main/java/com/example/myapplication/ConnectedCampus.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ConnectedCampus.java
@@ -7,12 +7,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.res.AssetManager;
+import android.graphics.BitmapFactory;
+import java.io.InputStream;
+
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
 
 public class ConnectedCampus extends Fragment {
+
+    private VrPanoramaView mVRPanoramaView1;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.campusconnected, container, false);
+        View view = inflater.inflate(R.layout.virtual_tour, container, false);
+        mVRPanoramaView1 = (VrPanoramaView) view.findViewById(R.id.vrPanoramaViewTest1);
+        loadPhotoSphere();
+        return view;
+    }
+    private void loadPhotoSphere() {
+        VrPanoramaView.Options options = new VrPanoramaView.Options();
+        InputStream inputStream = null;
+
+        AssetManager assetManager = getActivity().getAssets();
+        try {
+            inputStream = assetManager.open("vrtest.jpg");
+            options.inputType = VrPanoramaView.Options.TYPE_MONO;
+            mVRPanoramaView1.loadImageFromBitmap(BitmapFactory.decodeStream(inputStream), options);
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
