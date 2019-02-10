cc7f2dd680ebcaed84406a07cd94908d0a9e6928
Avdon Racki <avdon@racki.ca>
2019-02-09 21:10:32 -0500

    fix ace

--- MyApplication/app/src/main/java/com/example/myapplication/VirtualTourAce.java
+++ MyApplication/app/src/main/java/com/example/myapplication/VirtualTourAce.java
@@ -20,7 +20,7 @@ public class VirtualTourAce extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.individual_ace_360, container, false);
+        View view = inflater.inflate(R.layout.individual_ace, container, false);
         mVRPanoramaView = (VrPanoramaView) view.findViewById(R.id.vrPanoramaView);
         loadPhotoSphere();
 
